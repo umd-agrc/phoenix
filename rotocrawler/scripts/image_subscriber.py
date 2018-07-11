af127a4bdfddeb8c8e9efb3e69b4f16a77d2f30c
mdforbes500 <mforbes4@gmu.edu>
2018-07-10 22:38:37 -0400

    still broken
    needs fixing

--- rotocrawler/scripts/image_subscriber.py
+++ rotocrawler/scripts/image_subscriber.py
@@ -15,15 +15,22 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2 as cv
 
-def callback(data):
-    br = CvBridge()
-    rp.loginfo('recieving image')
-    cv.imshow("camera", br.imgmsg_to_cv2(data))
-    cv.waitKey(1)
+def callbackLeft(data):
+    br_left = CvBridge()
+    rp.loginfo('recieving image left')
+    cv.imshow('camera_driver', br_left.imgmsg_to_cv2(data))
+    cv.waitKey(5)
+
+def callbackRight(data):
+    br_right = CvBridge()
+    rp.loginfo('recieveing image right')
+    cv.imshow('camera_driver', br_right.imgmsg_to_cv2(data))
+    cv.waitKey(5)
 
 def cv_listener():
     rp.init_node('listener', anonymous=False)
-    rp.Subscriber('image_data', Image, callback)
+    rp.Subscriber('image_raw_left', Image, callbackLeft)
+    rp.Subscriber('image_raw_right', Image, callbackRight)
     rp.spin()
     cv.destroyAllWindows()
 
