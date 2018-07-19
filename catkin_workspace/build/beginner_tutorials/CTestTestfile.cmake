23aefed02b4b80f058c274ed0be79deabd79686c
mdforbes500 <mforbes4@gmu.edu>
2018-07-19 16:44:08 -0400

    Setup incorrectly. Shouldn't have included entire workspace. Now with just the project folder.

--- catkin_workspace/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mdforbes/catkin_workspace/src/beginner_tutorials
-# Build directory: /home/mdforbes/catkin_workspace/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_workspace/build/rotocrawler/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mdforbes/catkin_workspace/src/rotocrawler
-# Build directory: /home/mdforbes/catkin_workspace/build/rotocrawler
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
