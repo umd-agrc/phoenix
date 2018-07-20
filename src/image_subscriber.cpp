9828fd9da8a789debc0687f0780850471b3bb5e6
mdforbes500 <mforbes4@gmu.edu>
2018-07-19 21:10:22 -0400

    now with approximate time sync and stereo launch files.

--- src/image_subscriber.cpp
+++ src/image_subscriber.cpp
@@ -1,30 +1,45 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/sync_policies/approximate_time.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+void imageCallback(const sensor_msgs::ImageConstPtr& msg1, const sensor_msgs::ImageConstPtr& msg2)
 {
   try
   {
     ROS_INFO("Image Recieved");
-    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::imshow("left", cv_bridge::toCvShare(msg1, "bgr8")->image);
+    cv::imshow("right", cv_bridge::toCvShare(msg2, "bgr8")->image);
     cv::waitKey(30);
   }
   catch (cv_bridge::Exception& e)
   {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg1->encoding.c_str());
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg2->encoding.c_str());
   }
 }
 
 int main(int argc, char* argv[])
 {
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "sterso_image_proc");
   ros::NodeHandle nh;
   cv::namedWindow("view");
   cv::startWindowThread();
+  image_transport::SubscriberFilter left_sub, right_sub;
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+  left_sub.subscribe("camera/image", 1, imageCallback);
+  right_sub.subscribe("camera/image", 1, imageCallback);
+
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> ApproximatePolicy;
+  typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
+
+  boost::shared_ptr<ApproximateSync> approximate_sync;
+
+  approximate_sync.reset(new ApproximateSync(ApproximatePolicy(queue_size),left_sub,right_sub));
+  approximate_sync->registerCallback(boost::bind(callback, _1, _2));
+
   ros::spin();
   cv::destroyWindow("view");
 }
